24ab076558e94364aa1ba0a64611831b4d86a6f2
YoKar <yordankaravachev@gmail.com>
2017-01-31 21:41:10 +0200

    new

---
+++ 01.DataTypesNumeralTypesAndTypeConversion/03.ExactRealNumbers/03.ExactRealNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.ExactRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+            decimal num2 = 1;
+            for (int i = 0; i < counter; i++)
+            {
+
+                decimal num = decimal.Parse(Console.ReadLine());
+                num2 *= num;
+            }
+            Console.WriteLine(num2);
+        }
+    }
+}
