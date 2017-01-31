be1bb814fe273c70a73859ae605158eec3b1555a
YoKar <yordankaravachev@gmail.com>
2017-01-31 21:43:47 +0200

    new

---
+++ 01.DataTypesNumeralTypesAndTypeConversion/04. Transport/Transport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Transport
+{
+    class Transport
+    {
+        static void Main(string[] args)
+        {
+            int persones = int.Parse(Console.ReadLine());
+            double carCapacity = 4 + 8 + 12;
+            Console.WriteLine("{0}", Math.Ceiling(persones / carCapacity));
+        }
+    }
+}
