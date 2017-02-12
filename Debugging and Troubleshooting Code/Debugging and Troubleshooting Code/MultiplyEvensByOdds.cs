7fd811af8dd9694043a1cebd6693a0f7b3edc264
YoKar <yordankaravachev@gmail.com>
2017-02-12 14:39:22 +0200

    new

---
+++ Debugging and Troubleshooting Code/Debugging and Troubleshooting Code/MultiplyEvensByOdds.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging_and_Troubleshooting_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMultipleOfEvensAndOdds(Math.Abs(number)));
+        }
+
+        private static int GetMultipleOfEvensAndOdds(int a)
+        {
+            int result = SumOfEvensDigits(a) * SumOfOddsDigits(a);
+            return result;
+        }
+
+        private static int SumOfOddsDigits(int num1)
+        {
+            int temp1 = 0;
+            int temp2 = 0;
+            while (num1 > 0)
+            {
+                temp1 = num1 % 10;
+                if (temp1 % 2 != 0)
+                {
+                    temp2 += temp1;
+                }
+                num1 /= 10;
+            }
+            return temp2;
+        }
+
+        private static int SumOfEvensDigits(int num1)
+        {
+            int temp = 0;
+            int temp2 = 0;
+            while (num1 > 0)
+            {
+                temp = num1 % 10;
+                if (temp % 2 == 0)
+                {
+                    temp2 += temp;
+                }
+                num1 /= 10;
+            }
+            return temp2;
+        }
+    }
+}
