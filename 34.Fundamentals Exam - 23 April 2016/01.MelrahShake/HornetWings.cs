277fb095ce33b3045c7258e43a88394c54433180
Jordan <yordankaravachev@gmail.com>
2017-04-20 18:55:40 +0300

    Exams

---
+++ 34.Fundamentals Exam - 23 April 2016/01.MelrahShake/HornetWings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.MelrahShake
+{
+    class HornetWings
+    {
+        static void Main(string[] args)
+        {
+            var GivenFlaps = decimal.Parse(Console.ReadLine());
+            var DistanseForThousFlasps = decimal.Parse(Console.ReadLine());
+            var FlapsForBreak = decimal.Parse(Console.ReadLine());
+            var realMeters = (GivenFlaps / 1000) * DistanseForThousFlasps;
+            int seconds = (int)(GivenFlaps / 100);
+            seconds += ((int)(GivenFlaps / FlapsForBreak))*5;
+            Console.WriteLine("{0:f2} m.",realMeters);
+            Console.WriteLine("{0:f0} s.",seconds);
+        }
+    }
+}
---
+++ 34.Fundamentals Exam - 23 April 2016/02.Crossfire/HornetComm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Crossfire
+{
+    class HornetComm
+    {
+        static void Main(string[] args)
+        {
+            var inputLine = Console.ReadLine();
+            
+            
+            var BroadCastList = new List<string>();
+            var MeesegList = new List<string>();
+            while (inputLine!= "Hornet is Green")
+            {
+                var tokens = inputLine.Split(" ".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (tokens.Length>3|| tokens[2].Contains('@')|| tokens[2].Contains('!')|| tokens[2].Contains('$'))
+                {
+                    inputLine = Console.ReadLine();
+                    continue;
+                }
+               
+                var num = 0;
+                if (int.TryParse(tokens[0],out num))
+                {
+                    var FirstQuery =tokens[0].ToList();
+                    FirstQuery.Reverse();
+                    var secondQuery = tokens[2];
+                    MeesegList.Add(string.Join("", FirstQuery) + " -> " + secondQuery);
+                   
+                }
+                else
+                {
+                    var secondQuery = new List<char>();
+                    var FirstQuery = tokens[0];
+                    foreach (var charr in tokens[2])
+                    {
+                        secondQuery.Add(charr);
+                    }
+                    
+                    var CorectedList = new List<char>();
+                    foreach (var letter in secondQuery)
+                    {
+                        if (char.IsUpper(letter))
+                        {
+                            var let = char.ToLower(letter);
+                            CorectedList.Add(let);
+                        }
+                        else
+                        {
+                            var up = char.ToUpper(letter);
+                            CorectedList.Add(up);
+                        }
+                    }
+
+                    BroadCastList.Add(string.Join("", CorectedList)+" -> "+FirstQuery);
+                }
+                inputLine = Console.ReadLine();
+            }
+            Console.WriteLine("Broadcasts:");
+            if (BroadCastList.Count>0)
+            {
+               
+                foreach (var item in BroadCastList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("None");
+            }
+            Console.WriteLine("Messages:");
+            if (MeesegList.Count>0)
+            {
+                foreach (var item in MeesegList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("None");
+            }
+        }
+    }
+}
